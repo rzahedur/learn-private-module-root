2372eb90b58c1586f4634d78161631b5fdc79004
Zahedur <Zahedur@DESKTOP-4857TBK>
2026-04-25 22:32:33 -0400

    n

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-module "terraform-aws-s3-webapp" {
+module "s3-webapp" {
   source  = "app.terraform.io/Zenin/terraform-aws-s3-webapp/aws"
   name    = var.name
   region  = var.region
